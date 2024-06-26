247950f8e942824af5eefd21c88546698f7a34f3
Alireza Jahandoost <alireza.jhd2000@gmail.com>
2024-06-26 15:02:48 +0330

    Make Correction models

--- correction/models.py
+++ correction/models.py
@@ -1,3 +1,32 @@
+from django.contrib.auth.models import User
 from django.db import models
 
+
 # Create your models here.
+
+class QuestionType(models.Model):
+    name = models.CharField(max_length=100)
+
+
+class QuestionTypeData(models.Model):
+    TYPE_TPO = 1
+    TYPE_NEO = 2
+    TYPE_CHOICES = [
+        (TYPE_TPO, _("TPO")),
+        (TYPE_NEO, _("NEO")),
+    ]
+
+    question_type = models.ForeignKey(QuestionType, on_delete=models.CASCADE)
+    type_name = models.PositiveSmallIntegerField(choices=TYPE_CHOICES, null=True, blank=True)
+    type_number = models.PositiveIntegerField(null=True, blank=True)
+    data = models.TextField()
+
+
+class Correction(models.Model):
+    question_type_data = models.ForeignKey(QuestionTypeData, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    answer = models.TextField(blank=True, null=True)
+    correction = models.TextField(blank=True, null=True)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)

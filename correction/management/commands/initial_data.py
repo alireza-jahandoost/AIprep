7001473f30b4903baa8f1102bd4bbd74b2d92638
Alireza Jahandoost <alireza.jhd2000@gmail.com>
2024-07-02 15:53:12 +0330

    Convert initializers to commands
    convert initial_data.py and initial_test_data.py to commands

---
+++ correction/management/commands/initial_data.py
@@ -0,0 +1,44 @@
+import os
+import re
+import sys
+
+from django.core.management import BaseCommand
+
+from correction.models import QuestionTypeData, QuestionType
+
+
+class Command(BaseCommand):
+    help = "Adds exams' data to the database."
+
+    def handle(self, *args, **options):
+        TPO_DIR = "correction/data/TPO"
+        NEO_DIR = "correction/data/NEO"
+
+        sys.stdout.write('start\n')
+        sys.stdout.flush()
+        TOEFLIntegrated = QuestionType(name="TOEFL Integrated")
+        # TOEFLIndependent = QuestionType(name="TOEFL Independent")
+        TOEFLIntegrated.save()
+        # TOEFLIndependent.save()
+
+        for file in os.listdir(TPO_DIR):
+            f = open(os.path.join(TPO_DIR, file), "r", encoding="utf-8")
+            x = re.findall(r"\[(?P<section>\w+)]\n(?P<content>.*?)(?=\n\[|$)", f.read(), re.DOTALL)
+            reading = x[0][1]
+            listening = x[1][1]
+            QuestionTypeData.objects.create(question_type=TOEFLIntegrated,
+                                            type_name=QuestionTypeData.TYPE_CHOICES[0][0],
+                                            type_number=file.split(".")[0],
+                                            data={"reading": reading, "listening": listening})
+
+        for file in os.listdir(NEO_DIR):
+            f = open(os.path.join(NEO_DIR, file), "r", encoding="utf-8")
+            x = re.findall(r"\[(?P<section>\w+)]\n(?P<content>.*?)(?=\n\[|$)", f.read(), re.DOTALL)
+            reading = x[0][1]
+            listening = x[1][1]
+            QuestionTypeData.objects.create(question_type=TOEFLIntegrated,
+                                            type_name=QuestionTypeData.TYPE_CHOICES[1][0],
+                                            type_number=file.split(".")[0],
+                                            data={"reading": reading, "listening": listening})
+
+        sys.stdout.write('end\n')

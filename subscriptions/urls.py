40dd0417a5c52dc6896cd87e6765bc7101768842
Alireza Jahandoost <alireza.jhd2000@gmail.com>
2024-07-28 23:48:39 +0330

    Add show plans page and subscription ordering

--- subscriptions/urls.py
+++ subscriptions/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('request/', views.send_request, name='request'),
-    path('verify/', views.verify, name='verify'),
+    path('plans/', views.show_plans, name='subscription_plans'),
+    path('order/<int:plan_id>', views.order, name='subscription_order'),
+    path('verify/<int:plan_id>', views.verify, name='verify'),
 ]
--- subscriptions/views.py
+++ subscriptions/views.py
@@ -1,35 +1,43 @@
 from django.conf import settings
-from django.shortcuts import redirect
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import redirect, render, get_object_or_404
 from django.http import HttpResponse
 import requests
 import json
 
+from django.urls import reverse
+from django.utils.decorators import method_decorator
 
-#? sandbox merchant 
+from subscriptions.models import Plan, Payment
+
+#? sandbox merchant
 if settings.SANDBOX:
     sandbox = 'sandbox'
 else:
     sandbox = 'www'
 
-
 ZP_API_REQUEST = f"https://{sandbox}.zarinpal.com/pg/rest/WebGate/PaymentRequest.json"
 ZP_API_VERIFY = f"https://{sandbox}.zarinpal.com/pg/rest/WebGate/PaymentVerification.json"
 ZP_API_STARTPAY = f"https://{sandbox}.zarinpal.com/pg/StartPay/"
 
-amount = 1000  # Rial / Required
-description = "توضیحات مربوط به تراکنش را در این قسمت وارد کنید"  # Required
 phone = 'YOUR_PHONE_NUMBER'  # Optional
 # Important: need to edit for real server.
-CallbackURL = 'http://127.0.0.1:8000/subscriptions/verify/'
 
 
-def send_request(request):
+@login_required(login_url='login')
+def show_plans(request):
+    plans = Plan.objects.all()
+    return render(request, 'show_plans.html', {'plans': plans})
+
+@login_required(login_url='login')
+def order(request, plan_id):
+    plan = get_object_or_404(Plan, pk=plan_id)
+
     data = {
         "MerchantID": settings.MERCHANT,
-        "Amount": amount,
-        "Description": description,
-        "Phone": phone,
-        "CallbackURL": CallbackURL,
+        "Amount": plan.price,
+        "Description": f"خرید اشتراک {plan.plan_name} {plan.number_of_days} روزه",
+        "CallbackURL": request.build_absolute_uri(reverse('verify', args=[plan.id])),
     }
     data = json.dumps(data)
     # set content length by data
@@ -41,7 +49,7 @@ def send_request(request):
             response_json = response.json()
             authority = response_json['Authority']
             if response_json['Status'] == 100:
-                return redirect(ZP_API_STARTPAY+authority)
+                return redirect(ZP_API_STARTPAY + authority)
             else:
                 return HttpResponse('Error')
         return HttpResponse('response failed')
@@ -50,14 +58,16 @@ def send_request(request):
     except requests.exceptions.ConnectionError:
         return HttpResponse('Connection Error')
 
+@login_required(login_url='login')
+def verify(request, plan_id):
+    plan = get_object_or_404(Plan, pk=plan_id)
 
-def verify(request):
     authority = request.GET.get('Authority')
     status = request.GET.get('Status')
     if status == 'OK' and authority:
         data = {
             "MerchantID": settings.MERCHANT,
-            "Amount": amount,
+            "Amount": plan.price,
             "Authority": authority,
         }
         data = json.dumps(data)
@@ -69,6 +79,9 @@ def verify(request):
                 response_json = response.json()
                 reference_id = response_json['RefID']
                 if response_json['Status'] == 100:
+                    Payment.objects.create(plan=plan,
+                                           user=request.user,
+                                           ref_id=response_json['RefID'],)
                     return HttpResponse(f'successful , RefID: {reference_id}')
                 else:
                     return HttpResponse('Error')

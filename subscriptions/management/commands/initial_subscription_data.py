f1438cad5af4f7a7eb68c92a0abd2c4e38df4347
Alireza Jahandoost <alireza.jhd2000@gmail.com>
2024-07-28 19:42:01 +0330

    Make subscription data initialization command

---
+++ subscriptions/management/commands/initial_subscription_data.py
@@ -0,0 +1,41 @@
+import os
+import re
+import string
+import sys
+
+from django.core.management import BaseCommand
+
+from correction.models import QuestionTypeData
+from subscriptions.models import Plan
+
+
+class Command(BaseCommand):
+    help = "Adds subscription plans to the database."
+
+    def handle(self, *args, **options):
+        # Normal Plan
+        Plan.objects.create(plan_name="Normal",
+                            available_daily_corrections=2,
+                            correction_type=Plan.CORRECTION_TYPE_NORMAL,
+                            number_of_days=-1,
+                            price=0)
+
+        # Pro Plan
+        pro_prices = [1000, 2000, 3000]
+        pro_days = [30, 90, 180]
+        for i in range(len(pro_prices)):
+            Plan.objects.create(plan_name='Pro' + str(pro_days[i]),
+                                available_daily_corrections=2,
+                                correction_type=Plan.CORRECTION_TYPE_PRO,
+                                number_of_days=pro_days[i],
+                                price=pro_prices[i])
+
+        # Pro Plus Plan
+        pro_plus_prices = [1000, 2000, 3000]
+        pro_plus_days = [30, 90, 180]
+        for i in range(len(pro_prices)):
+            Plan.objects.create(plan_name='ProPlus' + str(pro_plus_days[i]),
+                                available_daily_corrections=4,
+                                correction_type=Plan.CORRECTION_TYPE_PRO,
+                                number_of_days=pro_plus_days[i],
+                                price=pro_plus_prices[i])

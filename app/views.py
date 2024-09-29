7891334a77a1515abc858cc6868d6a32c8f39866
Alireza Jahandoost <alireza.jhd2000@gmail.com>
2024-09-29 16:03:13 +0200

    Implement dashboard

--- app/views.py
+++ app/views.py
@@ -2,6 +2,7 @@
 """
 Copyright (c) 2019 - present AppSeed.us
 """
+import datetime
 from lib2to3.fixes.fix_input import context
 
 from django.contrib.auth.decorators import login_required
@@ -9,6 +10,10 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.template import loader
 from django.http import HttpResponse
 from django import template
+from django.utils import timezone
+
+from correction.helper_functions import get_number_of_today_corrections
+from subscriptions.helper_functions import get_current_plan_of_user, get_last_payment_of_user
 
 
 @login_required(login_url="/login/")
@@ -44,5 +49,16 @@ def pages(request):
 
 @login_required(login_url="/login/")
 def dashboard(request):
-    context = {'segment':'dashboard'}
+    user_plan = get_current_plan_of_user(request.user)
+    user_last_payment = get_last_payment_of_user(request.user)
+    context = {
+        'segment': 'dashboard',
+        'user_plan': user_plan,
+        'remaining_corrections': max(0, user_plan.available_daily_corrections - get_number_of_today_corrections(request.user)),
+        'corrections': request.user.correction_set.order_by('-created_at')[:5]
+    }
+    if user_last_payment:
+        context['start_of_plan'] = user_last_payment.created_at.strftime('%b %d')
+        context['end_of_plan'] = (user_last_payment.created_at + datetime.timedelta(days=user_plan.number_of_days)).strftime('%b %d')
+        context['remaining_days'] = ((user_last_payment.created_at + datetime.timedelta(days=user_plan.number_of_days)) - timezone.now()).days
     return render(request, "dashboard.html", context)
--- subscriptions/helper_functions.py
+++ subscriptions/helper_functions.py
@@ -2,9 +2,12 @@ import time
 
 from subscriptions.models import Plan
 
+def get_last_payment_of_user(user):
+    last_payment = user.payment_set.order_by('-created_at').first()
+    return last_payment
 
 def get_current_plan_of_user(user):
-    last_payment = user.payment_set.order_by('-created_at').first()
+    last_payment = get_last_payment_of_user(user)
     if (last_payment is not None) and (last_payment.created_at.timestamp() > time.time() - last_payment.plan.number_of_days * 24 * 3600):
         return last_payment.plan
     else:

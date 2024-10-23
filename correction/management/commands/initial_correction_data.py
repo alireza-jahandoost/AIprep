01247866caf573ed97f619ff0eeeae0188e13221
Alireza Jahandoost <alireza.jhd2000@gmail.com>
2024-10-23 23:18:20 +0200

    Improve initial_correction_data command
    Improve initial_correction_data command to support adding new
    question type data without making duplicates

--- correction/management/commands/initial_correction_data.py
+++ correction/management/commands/initial_correction_data.py
@@ -21,6 +21,11 @@ class Command(BaseCommand):
         printable_chars = set(string.printable)
 
         for file in os.listdir(TPO_DIR):
+            if QuestionTypeData.objects.filter(exam_type=QuestionTypeData.EXAM_TYPE_TOEFL_TASK1,
+                                            exam_db_name=QuestionTypeData.EXAM_DB_TPO,
+                                            exam_db_number=file.split(".")[0]).exists():
+                continue
+
             f = open(os.path.join(TPO_DIR, file), "r", encoding="utf-8")
             x = re.findall(r"\[(?P<section>\w+)]\n(?P<content>.*?)(?=\n\[|$)", f.read(), re.DOTALL)
 
@@ -32,6 +37,11 @@ class Command(BaseCommand):
                                             data={"reading": reading, "listening": listening})
 
         for file in os.listdir(NEO_DIR):
+            if QuestionTypeData.objects.filter(exam_type=QuestionTypeData.EXAM_TYPE_TOEFL_TASK1,
+                                            exam_db_name=QuestionTypeData.EXAM_DB_NEO,
+                                            exam_db_number=file.split(".")[0]).exists():
+                continue
+
             f = open(os.path.join(NEO_DIR, file), "r", encoding="utf-8")
             x = re.findall(r"\[(?P<section>\w+)]\n(?P<content>.*?)(?=\n\[|$)", f.read(), re.DOTALL)
             reading = ''.join(filter(lambda x: x in set(printable_chars), x[0][1]))
@@ -52,6 +62,11 @@ class Command(BaseCommand):
         printable_chars = set(string.printable)
 
         for file in os.listdir(NEO_DIR):
+            if QuestionTypeData.objects.filter(exam_type=QuestionTypeData.EXAM_TYPE_TOEFL_TASK2,
+                                            exam_db_name=QuestionTypeData.EXAM_DB_NEO,
+                                            exam_db_number=file.split(".")[0]).exists():
+                continue
+                
             f = open(os.path.join(NEO_DIR, file), "r", encoding="utf-8")
             reading = f.read()
             reading = ''.join(filter(lambda x: x in set(printable_chars), reading))
